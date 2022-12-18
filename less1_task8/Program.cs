d0592f4c02230938cd9bebc6e835259550bea448
Artem <temass2012@gmail.com>
2022-12-19 00:15:25 +0300

    урок 1 задача 8

---
+++ less1_task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+for (int index=1; index<=number; index++)
+{
+    if (Math.IEEERemainder(index,2) == 0)
+    {
+        System.Console.Write(index+" ");
+    }
+}
