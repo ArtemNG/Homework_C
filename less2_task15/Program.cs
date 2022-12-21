72779706da7cb9b859e47132dbd8168b195a312e
Artem <temass2012@gmail.com>
2022-12-22 01:29:13 +0300

    урок 2 задача 15

---
+++ less2_task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int numIN = System.Convert.ToInt32(System.Console.ReadLine());
+if (numIN < 8 && numIN > 0)
+{
+    if (numIN == 6 || numIN == 7)
+    {
+        System.Console.WriteLine("Да");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Это не день недели!");
+}
