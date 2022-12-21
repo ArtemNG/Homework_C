59608da966a3bf1e3013233651e91eb78b58914a
Artem <temass2012@gmail.com>
2022-12-22 01:28:44 +0300

    урок 2 задача 13

---
+++ less2_task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int numIN = System.Convert.ToInt32(System.Console.ReadLine());
+string numOut = System.Convert.ToString(numIN);
+if (numOut.Length < 3) 
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    numOut = numOut.Substring(2,1);   
+    System.Console.WriteLine(numOut);
+}
