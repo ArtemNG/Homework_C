a961b0b54b16489017c2c7dd9c664401c2e5b0f7
Artem <temass2012@gmail.com>
2022-12-22 01:28:18 +0300

    урок 2 задача 10

---
+++ less2_task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int numIN = 397; 
+string numOut = System.Convert.ToString(numIN);
+numOut = numOut.Substring(1,1);
+System.Console.WriteLine(numOut);
