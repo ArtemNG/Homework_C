82cae1d7928e647aef818df0191744c747e3c364
Artem <temass2012@gmail.com>
2022-12-25 02:30:10 +0300

    урок 3 задача 23

---
+++ less3_task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int num = System.Convert.ToInt32(System.Console.ReadLine());
+
+for (int i=1; num >= i; i++)
+{
+    if (i == num) 
+    {
+        System.Console.Write(Math.Pow(i,3));  // Чтоб запятую в конце не выводил :D
+    }
+    else
+    {
+        System.Console.Write(Math.Pow(i,3)+", ");
+    }     
+}
